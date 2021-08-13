d22a74f383412b3e947faca40a67e31031b9c350
MoonJuhan <codemike@naver.com>
2021-08-13 18:15:57 +0900

    fix: homebridge 함수 row 찾는 로직 수정

--- AccountBook/homebridge.js
+++ AccountBook/homebridge.js
@@ -2,20 +2,22 @@ const sheetHome = GSS.getSheetByName('스마트홈');
 
 const writeDHTSensor = ({ humidity, temperature }) => {
   const pinCell = sheetHome.createTextFinder('DHT22').findAll();
-  let pinRow = pinCell[0].getRow() + 3;
+  let pinRow = sheetHome.getLastRow() + 1;
   const pinColumn = pinCell[0].getColumn();
 
-  while (sheetHome.getRange(pinRow, pinColumn).getValue()) {
-    pinRow++;
-  }
-  pinRow--;
+  sheetHome.insertRowAfter(sheetHome.getLastRow());
 
+  console.log(
+    pinRow,
+    pinColumn,
+    sheetHome.getRange(pinRow, pinColumn).getValue()
+  );
 
   const time = new Date();
-
-  sheetHome.getRange(pinRow, pinColumn).setValue(time)
-  sheetHome.getRange(pinRow, pinColumn + 1).setValue(time)
-  sheetHome.getRange(pinRow, pinColumn + 2).setValue(temperature)
-  sheetHome.getRange(pinRow, pinColumn + 3).setValue(humidity)
-  
+  console.log(time);
+  console.log(humidity, temperature);
+  sheetHome.getRange(pinRow, pinColumn).setValue(time);
+  sheetHome.getRange(pinRow, pinColumn + 1).setValue(time);
+  sheetHome.getRange(pinRow, pinColumn + 2).setValue(temperature);
+  sheetHome.getRange(pinRow, pinColumn + 3).setValue(humidity);
 };
