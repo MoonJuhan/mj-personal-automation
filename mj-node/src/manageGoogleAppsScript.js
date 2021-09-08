296c52eaa243999e4690bd5df93c3c48ff15f06c
MoonJuhan <codemike@naver.com>
2021-09-08 18:26:53 +0900

    fix: mj-node GoogleAppsScript 관련 오류 수정

--- mj-node/src/manageGoogleAppsScript.js
+++ mj-node/src/manageGoogleAppsScript.js
@@ -4,34 +4,34 @@ import { telegramSend } from './manageTelegram'
 
 let _auth
 
-const TOKEN = {
-  access_token: process.env.GAS_ACCESS_TOKEN,
-  refresh_token: process.env.GAS_REFRESH_TOKEN,
-  scope:
-    'https://www.googleapis.com/auth/spreadsheets https://www.googleapis.com/auth/script.projects',
-  token_type: 'Bearer',
-  expiry_date: process.env.GAS_EXPIRY_DATE,
-}
-
-const CREDENTIALS = {
-  installed: {
-    client_id: process.env.GAS_CLIENT_ID,
-    project_id: process.env.GAS_PROJECT_ID,
-    auth_uri: 'https://accounts.google.com/o/oauth2/auth',
-    token_uri: 'https://oauth2.googleapis.com/token',
-    auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
-    client_secret: process.env.GAS_CLIENT_SECRET,
-    redirect_uris: ['urn:ietf:wg:oauth:2.0:oob', 'http://localhost'],
-  },
-}
-
 const authorize = () => {
+  const CREDENTIALS = {
+    installed: {
+      client_id: process.env.GAS_CLIENT_ID,
+      project_id: process.env.GAS_PROJECT_ID,
+      auth_uri: 'https://accounts.google.com/o/oauth2/auth',
+      token_uri: 'https://oauth2.googleapis.com/token',
+      auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
+      client_secret: process.env.GAS_CLIENT_SECRET,
+      redirect_uris: ['urn:ietf:wg:oauth:2.0:oob', 'http://localhost'],
+    },
+  }
   const { client_secret, client_id, redirect_uris } = CREDENTIALS.installed
+
   const oAuth2Client = new google.auth.OAuth2(
     client_id,
     client_secret,
     redirect_uris[0]
   )
+
+  const TOKEN = {
+    access_token: process.env.GAS_ACCESS_TOKEN,
+    refresh_token: process.env.GAS_REFRESH_TOKEN,
+    scope:
+      'https://www.googleapis.com/auth/spreadsheets https://www.googleapis.com/auth/script.projects',
+    token_type: 'Bearer',
+    expiry_date: process.env.GAS_EXPIRY_DATE,
+  }
 
   oAuth2Client.setCredentials(TOKEN)
   _auth = oAuth2Client
@@ -39,6 +39,7 @@ const authorize = () => {
 
 const callAppsScript = (parameters, callback) => {
   if (!_auth) authorize()
+
   const script = google.script({ version: 'v1', auth: _auth })
 
   script.scripts.run(
@@ -69,6 +70,7 @@ const callAppsScript = (parameters, callback) => {
 const routineFunc = async () => {
   const data = await getSensorData()
   console.log(data)
+
   callAppsScript(data, (suc, res) => {
     if (!suc) telegramSend('ESP8266 Error')
 
