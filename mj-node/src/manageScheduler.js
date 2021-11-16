c7974c6d02d3638bd9984ad7c6c1256419f7e14e
MoonJuhan <codemike@naver.com>
2021-11-16 14:32:09 +0900

    fix: mj-node 스케쥴러 평일만 실행되게 수정

--- mj-node/src/manageScheduler.js
+++ mj-node/src/manageScheduler.js
@@ -2,12 +2,12 @@ import schedule from 'node-schedule'
 import { getRentAnnouncement } from './manageCrawling'
 
 const setScheduler = () => {
-  const schedule0930 = schedule.scheduleJob('00 30 09 * * *', () => {
+  const schedule0930 = schedule.scheduleJob('00 30 09 * * 1-5', () => {
     console.log('09 30 실행')
     getRentAnnouncement()
   })
 
-  const schedule1800 = schedule.scheduleJob('00 00 18 * * *', () => {
+  const schedule1800 = schedule.scheduleJob('00 00 18 * * 1-5', () => {
     console.log('18 00 실행')
     getRentAnnouncement()
   })
